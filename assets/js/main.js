3d7b76168afcb9efbc8ccf6bcb092100f0c24a26
Anayn Pavez <nanyn.pavez@gmail.com>
2017-07-11 10:19:00 -0300

    ajax creation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,20 @@
 /* Acá va todo el jquery */
+var token = "8ac5835e9a61008fbe7a74c0983fa98927c729702025a54520b53429f70f7afd", //token
+	userid= d5acb727057a70db8f0712c984060f7b9ff3d91522490311b8c3103d14100273, // client id
+	num_trab = 12; //cantidad de trabajos
+
+$.ajax({
+	url: 'https://api.dribbble.com/v1/'+ userid +'/simplebits',
+	type: 'GET, ',
+	dataType: 'jsonp',
+	data: {access_token:token, count:num_photos},
+})
+.done(function() {
+	console.log("success");
+})
+.fail(function() {
+	console.log("error");
+})
+.always(function() {
+	console.log("complete");
+});
